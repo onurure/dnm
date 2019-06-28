61d842891bb991ad503288b7858701421775140d
Onur <odtulufb@hotmail.com>
2019-06-28 11:55:59 +0300

    a

---
+++ deneme-module/tests/Feature/DenemeModuleTest.php
@@ -0,0 +1,10 @@
+<?php namespace Onurure\DenemeModule\Test\Feature;
+
+class DenemeModuleTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
